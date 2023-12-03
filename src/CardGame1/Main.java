bb0e09af94a7c361b1a1fd82758983252012c91b
Aakaive <wonseok031@gmail.com>
2023-12-04 01:05:23 +0900

    main의 기능을 메소드로 분리하는 등의 코드 개선

--- src/CardGame1/Main.java
+++ src/CardGame1/Main.java
@@ -6,102 +6,84 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Integer[] deck = {1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8};
+        List<Integer> Card = shuffleCard(deck);
+        int turn = 1;
+        int remainCard = Card.size();
 
-        /*
-        List<Integer> card = Arrays.asList(deck);
-        Collections.shuffle(card);
-        List<Integer> table = new ArrayList<>(card.subList(0, 18));
-        int numX1 = 0;
-        int numY1 = 0;
-        int numX2 = 0;
-        int numY2 = 0;
-        int numIdx1 = 0;
-        int numIdx2 = 0;
-        int open = 0;
-        int remain = 24;
+        while(true){
+            showCardTable(Card, Command.BLIND);
+            System.out.println("<시도: "+turn+", 남은 카드: "+remainCard+"> 좌표를 두 번 입력하세요.");
+            System.out.print("입력 1? ([1~3] [1~6]) : ");
+            int selectedRow1 = sc.nextInt();
+            int selectedCol1 = sc.nextInt();
+            int selectedCardIdx1 = (selectedRow1-1)*6 + (selectedCol1-1);
+            System.out.print("입력 2? ([1~3] [1~6]) : ");
+            int selectedRow2 = sc.nextInt();
+            int selectedCol2 = sc.nextInt();
+            int selectedCardIdx2 = (selectedRow2-1)*6 + (selectedCol2-1);
 
-        while(true) {
-            int count = 0;
-            for(int i : table) {
-                if(i == 0) {
-                    System.out.print("  ");
-                }
-                else {
-                    System.out.print("X ");
-                }
-                if(count == 5 || count == 11 || count == 17) System.out.println("");
-                count++;
-            }
-
-            System.out.println("<시도 "+ open + ", 남은 카드: "+remain+"> 좌표를 두 번 입력하세요:");
-            System.out.println("입력1 ?([숫자1 : 1~3] [숫자2: 1~6])");
-            numY1 = sc.nextInt();
-            numX1 = sc.nextInt();
-            System.out.println("입력2 ?([숫자1 : 1~3] [숫자2: 1~6])");
-            numY2 = sc.nextInt();
-            numX2 = sc.nextInt();
-
-            if(numX1 > 6 || numY1 > 3 || numX2 > 6 || numY2 > 3) {
-                System.out.println("잘못된 위치입니다!!");
+            if(selectedCol1 > 6 || selectedRow1 > 3 || selectedCol2 > 6 || selectedRow2 > 3) {
+                System.out.println("\n[잘못된 위치입니다!!]\n");
                 continue;
             }
-            else if(numX1 == numX2 && numY1 == numY2){
-                System.out.println("같은 위치입니다!!");
+            else if(selectedCol1 == selectedCol2 && selectedRow1 == selectedRow2){
+                System.out.println("\n[같은 위치입니다!!]\n");
                 continue;
             }
-            else if(table.get(numIdx1) == 0 || table.get(numIdx2) == 0) {
+            else if(Card.get(selectedCardIdx1) == 0 || Card.get(selectedCardIdx2) == 0) {
                 System.out.println("\n[이미 맞춘 카드입니다!!]\n");
                 continue;
             }
+            openCard(Card, selectedCardIdx1, selectedCardIdx2);
+            turn++;
 
-            open++;
-
-            numIdx1 = numX1-1 + (numY1-1) * 6;
-            numIdx2 = numX2-1 + (numY2-1) * 6;
-
-            count = 0;
-            for(int i : table) {
-                if(count == numIdx1 || count == numIdx2) {
-                    System.out.print(i+" ");
-                }
-                else {
-                    if(i == 0) {
-                        System.out.print("  ");
-                    }
-                    else {
-                        System.out.print("X ");
-                    }
-                }
-                if(count == 5 || count == 11 || count == 17) System.out.println("");
-                count++;
+            if(Card.get(selectedCardIdx1) == Card.get(selectedCardIdx2)) {
+                Card.set(selectedCardIdx1, 0);
+                Card.set(selectedCardIdx2, 0);
+                remainCard -= 2;
             }
 
-            if(table.get(numIdx1) == table.get(numIdx2)) {
-                table.set(numIdx1, 0);
-                table.set(numIdx2, 0);
-                remain -= 2;
+            if(Card.size() - Collections.frequency(Card, 0) == Card.stream().distinct().count()-1) {
+                showCardTable(Card, Command.CHECK);
+                System.out.println("축하합니다!! [게임종료]");
+                break;
             }
-
-            System.out.println();
-
-            if(Collections.frequency(table, 0) >= 2) {
-                if(table.size() - Collections.frequency(table, 0) == table.stream().distinct().count()-1) {
-                    count = 0;
-                    for(int i : table) {
-                        if(i == 0){
-                            System.out.print(" ");
-                        }
-                        else {
-                            System.out.print(i+" ");
-                        }
-                        if(count == 5 || count == 11 || count == 17) System.out.println("");
-                        count++;
-                    }
-                    System.out.println("축하합니다! [게임 종료]");
-                    break;
-                }
-            }
-        }*/
-
+        }
     }
+
+    private static List<Integer> shuffleCard(Integer[] deck){
+        List<Integer> listDeck = Arrays.asList(deck);
+        Collections.shuffle(listDeck);
+        List<Integer> Card = new ArrayList<>(listDeck.subList(0, 18));
+        return Card;
+    }
+
+    private static void showCardTable(List<Integer> Card, Command cmd){
+        for(int idx = 0; idx < Card.size(); idx++){
+            if(cmd == Command.BLIND){
+                System.out.print(Card.get(idx) == 0 ? " " : "X");
+            }
+            else if(cmd == Command.CHECK){
+                System.out.print(Card.get(idx) == 0 ? " " : Card.get(idx));
+            }
+
+            if((idx+1)%6 == 0) System.out.println();
+            else System.out.print(" ");
+        }
+    }
+
+    private static void openCard(List<Integer> Card, int selectedCard1, int selectedCard2){
+        for(int idx = 0; idx < Card.size(); idx++){
+            if(selectedCard1 == idx || selectedCard2 == idx) System.out.print(Card.get(idx));
+            else System.out.print(Card.get(idx) == 0 ? " " : "X");
+
+            if((idx+1)%6 == 0) System.out.println();
+            else System.out.print(" ");
+        }
+        System.out.println();
+    }
+}
+
+enum Command {
+    BLIND, CHECK
 }
